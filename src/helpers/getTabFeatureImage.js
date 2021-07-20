eddda2ffc0f9a42467772f2c48bddff043fd8d84
Krishna Suwal <58679177+krishna-suwal@users.noreply.github.com>
2021-07-20 11:51:50 +0545

    Added a util function: getTabFeatureImage

---
+++ src/helpers/getTabFeatureImage.js
@@ -0,0 +1,30 @@
+function getTabFeatureImage(tab) {
+	return new Promise((resolve) => {
+		chrome.tabs.executeScript(
+			tab.id,
+			{
+				code: `document.querySelector('[property="og:image"]') ? document.querySelector('[property="og:image"]').getAttribute('content') : ''`,
+			},
+			(og_image_url) => {
+				const lastErr = chrome.runtime.lastError;
+
+				if (lastErr)
+					console.log(
+						'tab: ' + tab.id + ' lastError: ' + JSON.stringify(lastErr)
+					);
+
+				if (Array.isArray(og_image_url)) {
+					og_image_url = og_image_url[0];
+				}
+
+				if (og_image_url && typeof og_image_url === 'string') {
+					resolve(og_image_url);
+				} else {
+					resolve(tab.favIconUrl);
+				}
+			}
+		);
+	});
+}
+
+export default getTabFeatureImage;
